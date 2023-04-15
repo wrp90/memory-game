4e1e8911ac10759e38c96a88b32a01aaef354457
Will Papp <wpapp90@gmail.com>
2023-04-15 13:19:16 -0500

    can now flip cards

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MemoryCard.css";
 import '../App.css';
 
 const MemoryCard = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const clickHandler = () => {
+        setIsFlipped(!isFlipped)
+    }
+    const memoryCardInnerClass = isFlipped ? "MemoryCardInner flipped": "MemoryCardInner";
+
     return (
-        <div className="MemoryCard">
-            <div className="Memory-Card-Inner">
+        <div className="MemoryCard" onClick={clickHandler}>
+            <div className={memoryCardInnerClass}>
                 <div className="MemoryCardBack">
                     <img className="LogoBack" src="https://d6rlxajeumvxd.cloudfront.net/images/logos/dc-logo.svg"></img>
                 </div>
