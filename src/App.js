4e5d9a894c308b07f490e014ea34900aa49fa3fd
Will Papp <wpapp90@gmail.com>
2023-04-18 20:13:58 -0500

    added logic for rules of the game

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
 import './App.css';
 import MemoryCard from "./components/MemoryCard";
 import './components/MemoryCard.css';
+import { useState } from 'react';
 
 function App() {
+  const generateDeck = () => {
+    const symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø'];
+    let deck = [];
+
+    for (let i = 0; i < 16; i++) {
+      const card = {
+        isFlipped: false,
+        symbol: symbols[i % 8]
+      }
+      deck.push(card);
+    }
+    shuffle(deck);
+    return deck
+  }
+
+  const shuffle = (a) => {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
+  }
+
+  const pickCard = (cardIndex) => {
+    if (deck[cardIndex].isFlipped) return;
+    const cardToFlip = { ...deck[cardIndex], isFlipped: true };
+    let newPickedCards = pickedCards.concat(cardIndex);
+    const newDeck = deck.map((card, index) => {
+      if (cardIndex === index) {
+        return cardToFlip
+      }
+      return card
+    });
+    if (newPickedCards.length === 2) {
+      const card1Index = newPickedCards[0];
+      const card2Index = newPickedCards[1];
+      if (newDeck[card1Index].symbol !== newDeck[card2Index].symbol) {
+        setTimeout(() => unflipCards(card1Index, card2Index), 1000);
+      }
+      newPickedCards = [];
+    }
+    setDeck(newDeck);
+    setPickedCards(newPickedCards);
+  }
+
+  const unflipCards = (card1Index, card2Index) => {
+    const card1 = { ...deck[card1Index], isFlipped: false }
+    const card2 = { ...deck[card2Index], isFlipped: false }
+
+    const newDeck = deck.map((card, index) => {
+      if (card.symbol === card1.symbol) {
+        return card1
+      }
+      if (card.symbol === card2.symbol) {
+        return card2
+      }
+      return card
+    });
+    setDeck(newDeck);
+  }
+  const [deck, setDeck] = useState(generateDeck());
+  const [pickedCards, setPickedCards] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,30 +79,14 @@ function App() {
         <h2 className="subtitle">Match cards to win</h2>
       </header>
       <div className="card-container">
-        <div className="cell"> 
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-        </div>
-        <div className="cell">
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-        </div>
-        <div className="cell">
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-        </div>
-        <div className="cell">
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-          <MemoryCard />
-        </div>
+        {deck.map((card, index) =>
+          <MemoryCard
+            key={index}
+            symbol={card.symbol}
+            isFlipped={card.isFlipped}
+            pickCard={() => pickCard(index)}
+          />
+        )}
       </div>
     </div>
   );
--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -1,23 +1,16 @@
-import React, { useState } from "react";
 import "./MemoryCard.css";
 import '../App.css';
 
-const MemoryCard = () => {
-    const [isFlipped, setIsFlipped] = useState(false);
-
-    const clickHandler = () => {
-        setIsFlipped(!isFlipped)
-    }
+const MemoryCard = ({symbol, isFlipped, pickCard}) => {
     const memoryCardInnerClass = isFlipped ? "MemoryCardInner flipped": "MemoryCardInner";
-
     return (
-        <div className="MemoryCard" onClick={clickHandler}>
+        <div className="MemoryCard" onClick={pickCard}>
             <div className={memoryCardInnerClass}>
                 <div className="MemoryCardBack">
                     <img className="LogoBack" src="https://d6rlxajeumvxd.cloudfront.net/images/logos/dc-logo.svg"></img>
                 </div>
                 <div className="MemoryCardFront">
-                    ∆
+                    {symbol}
                 </div>
             </div>
         </div>
